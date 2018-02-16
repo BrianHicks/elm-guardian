e4ad42ba0b11284671bacb9a890ceda6d3ad68e2
Brian Hicks <brian@brianthicks.com>
2018-02-16 14:29:39 -0600

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Guardian.Repo
+alias Guardian.Packages.{Package, Version}
+
+parser = %Package{name: "elm-tools/parser", source: "https://github.com/elm-tools/parser"} |> Repo.insert!
+%Version{package: parser, major: 2, minor: 0, patch: 0, compiler: "0.18"} |> Repo.insert!
+%Version{package: parser, major: 2, minor: 0, patch: 1, compiler: "0.18"} |> Repo.insert!
+
+primitives = %Package{name: "elm-tools/parser-primitives", source: "https://github.com/elm-tools/parser-primitives"} |> Repo.insert!
+%Version{package: primitives, major: 1, minor: 0, patch: 0, compiler: "0.18"} |> Repo.insert!
+
