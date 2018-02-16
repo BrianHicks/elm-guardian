b3f24fecbab46481982db687c193d41a943b242d
Brian Hicks <brian@brianthicks.com>
2018-02-16 14:29:44 -0600

    add absinthe

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Guardian.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe_ecto, "~> 0.1.0"},
+      {:absinthe_plug, "~> 1.3.0"}
     ]
   end
 
