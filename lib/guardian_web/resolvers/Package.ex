ac5ad3aaaffdbf2db22bbcb52f0e59c0f104432c
Brian Hicks <brian@brianthicks.com>
2018-02-16 16:29:04 -0600

    add version relation

--- lib/guardian_web/resolvers/Package.ex
+++ lib/guardian_web/resolvers/Package.ex
@@ -1,7 +1,7 @@
 defmodule GuardianWeb.Resolvers.Package do
   alias Guardian.Packages
 
-  def all_packages(_root, _args, _info) do
+  def list(_root, _args, _info) do
     {:ok, Packages.list_packages()}
   end
 end
--- lib/guardian_web/schema.ex
+++ lib/guardian_web/schema.ex
@@ -1,17 +1,29 @@
 defmodule GuardianWeb.Schema do
   use Absinthe.Schema
+  use Absinthe.Ecto, repo: Guardian.Repo
 
-  alias Guardian.Packages.{Package}
   alias GuardianWeb.Resolvers
 
   object :package do
     field(:name, non_null(:string))
     field(:source, non_null(:string))
+    field(
+      :versions,
+      non_null(list_of(non_null(:version))),
+      resolve: assoc(:versions)
+    )
+  end
+
+  object :version do
+    field(:major, non_null(:integer)) # TODO: make these together a fancy scalar
+    field(:minor, non_null(:integer))
+    field(:patch, non_null(:integer))
+    field(:compiler, non_null(:string)) # TODO: make an enum
   end
 
   query do
-    field :all_packages, non_null(list_of(non_null(:package))) do
-      resolve(&Resolvers.Package.all_packages/3)
+    field :packages, non_null(list_of(non_null(:package))) do
+      resolve(&Resolvers.Package.list/3)
     end
   end
 end
