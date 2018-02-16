0ff61a502e7d042dc99b425da5f8a025f411ab61
Brian Hicks <brian@brianthicks.com>
2018-02-16 15:05:26 -0600

    catch up on formatting

--- lib/guardian_web/resolvers/Package.ex
+++ lib/guardian_web/resolvers/Package.ex
@@ -2,6 +2,6 @@ defmodule GuardianWeb.Resolvers.Package do
   alias Guardian.Packages
 
   def all_packages(_root, _args, _info) do
-    {:ok, Packages.list_packages}
+    {:ok, Packages.list_packages()}
   end
 end
--- lib/guardian_web/schema.ex
+++ lib/guardian_web/schema.ex
@@ -5,13 +5,13 @@ defmodule GuardianWeb.Schema do
   alias GuardianWeb.Resolvers
 
   object :package do
-    field :name, non_null(:string)
-    field :source, non_null(:string)
+    field(:name, non_null(:string))
+    field(:source, non_null(:string))
   end
 
   query do
     field :all_packages, non_null(list_of(non_null(:package))) do
-      resolve &Resolvers.Package.all_packages/3
+      resolve(&Resolvers.Package.all_packages/3)
     end
   end
 end
