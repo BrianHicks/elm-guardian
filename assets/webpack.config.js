5f36e0fb189eda82a37ae76e9108b7f249cf122d
Brian Hicks <brian@brianthicks.com>
2018-02-16 22:24:39 -0600

    uglify output

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   module: {
@@ -15,6 +16,7 @@ module.exports = {
       }
     ]
   },
+  plugins: [new UglifyJsPlugin()],
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
